1c8c67cd3281debc5513448b705adf21ea3dc58e
EunsukRyu <87590254+EunsukRyu@users.noreply.github.com>
2026-05-08 15:43:12 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+# main.py
 import streamlit as st
+import folium
+from strimlit-folium import st_folium
+import pandas as pd
 
+# 1. 웹 페이지 설정
+st.set_page_config(page_title="남동고 등산 메이트", layout="wide")
 st.text("안녕~~ 남동고등학교 여러분.. 첫 페이지를 만드셨습니다.")
