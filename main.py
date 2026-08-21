734c1ac7a7136a65bf96a7cbab1ccc5080e45dcc
EunsukRyu <87590254+EunsukRyu@users.noreply.github.com>
2026-08-21 16:18:17 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import streamlit as st
 import folium
 from streamlit_folium import st_folium
 import pandas as pd
+from PIL import Image, ImageOps
 
 # 1. 웹 페이지 설정
 st.set_page_config(page_title="남동고 등산 메이트", layout="wide")
@@ -161,10 +162,25 @@ with col2:
         for idx, row in filtered_df.iterrows():
             st.write(f"📍 **{row['위치명']}**")
             img_path = row['이미지']
+            #사진 방향 정보 수정
             if os.path.exists(img_path):
-                st.image(img_path, caption=row['위치명'], use_container_width=True)
+                try:
+                    img = Image.open(img_path)
+                    # EXIF 회전 정보를 실제 이미지 방향에 적용
+                    img = ImageOps.exif_transpose(img)
+                    st.image(
+                        img,
+                        caption=row['위치명'],
+                        use_container_width=True
+                    )
+                except Exception:
+                    st.caption("*(해당 지점 이미지 파일을 불러올 수 없습니다.)*")
+            #if os.path.exists(img_path):
+            #    st.image(img_path, caption=row['위치명'], use_container_width=True)
+            #else:
+            #    st.caption("📷 *(해당 지점 이미지 파일 준비 중)*")            
             else:
-                st.caption("📷 *(해당 지점 이미지 파일 준비 중)*")
+                st.caption("*(해당 지점 이미지 파일 준비 중)*")
     else:
         st.info("👈 왼쪽 사이드바에서 특정 코스(A~E)를 선택하면 예상 소요시간, 주의사항 및 포인트별 사진을 상세히 보실 수 있습니다.")
         
