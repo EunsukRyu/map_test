525c854098d456de5c009158f325d225f072b4bc
EunsukRyu <87590254+EunsukRyu@users.noreply.github.com>
2026-08-21 16:27:14 +0900

    Update main.py

--- main.py
+++ main.py
@@ -162,6 +162,7 @@ with col2:
         for idx, row in filtered_df.iterrows():
             st.write(f"📍 **{row['위치명']}**")
             img_path = row['이미지']
+            
             #사진 방향 정보 수정
             if os.path.exists(img_path):
                 try:
@@ -174,13 +175,11 @@ with col2:
                         use_container_width=True
                     )
                 except Exception:
-                    st.caption("*(해당 지점 이미지 파일을 불러올 수 없습니다.)*")
+                    st.caption("📷 *(해당 지점 이미지 파일을 불러올 수 없습니다.)*")
             #if os.path.exists(img_path):
             #    st.image(img_path, caption=row['위치명'], use_container_width=True)
-            #else:
-            #    st.caption("📷 *(해당 지점 이미지 파일 준비 중)*")            
             else:
-                st.caption("*(해당 지점 이미지 파일 준비 중)*")
+                st.caption("📷 *(해당 지점 이미지 파일 준비 중)*")
     else:
         st.info("👈 왼쪽 사이드바에서 특정 코스(A~E)를 선택하면 예상 소요시간, 주의사항 및 포인트별 사진을 상세히 보실 수 있습니다.")
         
